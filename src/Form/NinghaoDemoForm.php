d34a92679411142860f0c79e730abaaa0e414e00
Phoenix <117663444@qq.com>
2016-11-19 12:23:54 +0800

    创建一个表单

---
+++ src/Form/NinghaoDemoForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\ninghao_demo\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class NinghaoDemoForm extends FormBase {
+  public function getFormId() {
+    return 'ninghao_demo_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+  }
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+}
