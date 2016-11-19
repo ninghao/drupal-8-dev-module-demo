46ef059a0e8eef03e944e05e7b084b2eff3a0bd9
Phoenix <117663444@qq.com>
2016-11-19 12:29:09 +0800

    表单验证

--- src/Form/NinghaoDemoForm.php
+++ src/Form/NinghaoDemoForm.php
@@ -4,6 +4,7 @@ namespace Drupal\ninghao_demo\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Unicode;
 
 class NinghaoDemoForm extends FormBase {
   public function getFormId() {
@@ -25,7 +26,11 @@ class NinghaoDemoForm extends FormBase {
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
+    if (!$form_state->isValueEmpty('name')) {
+      if (Unicode::strlen($form_state->getValue('name')) < 2) {
+        $form_state->setErrorByName('name', t('姓名 必须使用两个或以上的字符'));
+      }
+    }
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
