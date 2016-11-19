8c2e0c0c7e4a97750e56e9147238dd69b6532b8b
Phoenix <117663444@qq.com>
2016-11-19 11:18:51 +0800

    路由的参数

--- src/Controller/NinghaoDemoController.php
+++ src/Controller/NinghaoDemoController.php
@@ -4,9 +4,9 @@ namespace Drupal\ninghao_demo\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class NinghaoDemoController extends ControllerBase {
-  public function page_content() {
+  public function page_content($page_id) {
     return [
-      '#markup' => t('这是一个来自模块的自定义页面'),
+      '#markup' => t('这是一个来自模块的自定义页面：@page_id', ['@page_id' => $page_id]),
     ];
   }
 }
