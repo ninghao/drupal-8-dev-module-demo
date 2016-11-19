c095e819612416c06c872ec26d8d91bb6efe514d
Phoenix <117663444@qq.com>
2016-11-19 13:56:27 +0800

    区块配置

--- src/Plugin/Block/NinghaoDemoBlock.php
+++ src/Plugin/Block/NinghaoDemoBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\ninghao_demo\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'Demo' Block
@@ -19,6 +20,25 @@ class NinghaoDemoBlock extends BlockBase {
     return [
       '#markup' => t('hello ~'),
     ];
+  }
+
+  public function defaultConfiguration() {
+    return [
+      'name' => '',
+    ];
+  }
+
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('姓名'),
+      '#default_name' => $this->configuration['name'],
+    ];
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['name'] = $form_state->getValue('name');
   }
 
 }
